91d8a4810aadba7aea6aa9bed58608d2e0abc49d
rem1xzov <b.programc@gmail.com>
2026-03-04 20:15:19 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.HttpOverrides;
 using MyLittlePony_Conexy.Application.Services;
 using MyLittlePony_Conexy.Infrastructure;
 using MyLittlePony_Conexy.Infrastructure.Repositories;
@@ -12,13 +11,6 @@ var configuration = builder.Configuration;
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownNetworks.Clear();
-    options.KnownProxies.Clear();
-});
 
 builder.Services.AddDbContext<QuizDbContext>(options =>
 {
@@ -33,8 +25,6 @@ builder.Services.AddScoped<IQuizService, QuizService>();
 
 var app = builder.Build();
 
-app.UseForwardedHeaders();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -44,19 +34,10 @@ if (app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<QuizDbContext>();
-    await dbContext.Database.MigrateAsync();
-
-    var seedData = configuration.GetValue("SeedData", true);
-    if (seedData)
-    {
-        await QuizSeeder.SeedAsync(dbContext);
-    }
+    await QuizSeeder.SeedAsync(dbContext);
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
